d0f07288a3a8d9631c88c9ed702de1b86a48fdc5
jaide <jayzawrotny@gmail.com>
2024-04-09 20:28:01 -0400

    Test secret encryption #3

--- nix/secrets/default.nix
+++ nix/secrets/default.nix
@@ -1,3 +1,9 @@
-{
-  kagiSessionToken = "test value";
-}
+age-encryption.org/v1
+-> ssh-ed25519 mrSMKw ocPCVpg+352lkSkLBHam+T7N04jQjbMZBdDtcTte1lE
+Vjy071W7ruKWegVrgbl0Tcq60pXNZohgao7DuqpewqE
+-> R9j!-grease
+p8ZMWvS6D6TxBNXs0PM56/nXO1CW2zg8MzujLLcdSv+7eXm22iv5wLO+NlC981+Z
+14O8QWb0d1obXROSHujoyaru0Pk
+--- /DbUZvNfrhOAHZcLD+EQ83emnddbd43iSQ8HjuG8kkM
+˜∂Ÿºå*Öæ¯|ﬁÑFu”’ATÈÖÔ—r=RçõRá◊p¸9Pº:É>l&¢ç©Å
+Œ<‹v¢\€‹Â¸ªËNDÜ⁄`
