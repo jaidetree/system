2908a2aeb02caa50d5e4227a86aa73820117ef13
jaide <jayzawrotny@gmail.com>
2025-01-20 20:12:04 -0500

    Update starship

--- nix/home/starship.nix
+++ nix/home/starship.nix
@@ -7,7 +7,35 @@ in
     enable = true;
     enableTransience = true;
     settings = {
-      format = "$all";
+      format = ''
+        $directory$line_break$all$line_break$character
+      '';
+      cmd_duration = {
+        disabled = true;
+      };
+      directory = {
+        truncate_to_repo = false;
+        fish_style_pwd_dir_length = 1;
+      };
+      git_branch = {
+        format = "[$symbol$branch(:$remote_branch)]($style) ";
+      };
+      nix_shell = {
+        format = "[$symbol]($style)";
+        symbol = "❄️ ";
+      };
+      nodejs = {
+        disabled = true;
+      };
+      ocaml = {
+        disabled = true;
+      };
+      shell = {
+        disabled = false;
+        fish_indicator = "";
+        zsh_indicator = "󰬡";
+      };
+
       palette = "catppuccin_${flavor}";
     } // builtins.fromTOML (builtins.readFile
       (pkgs.fetchFromGitHub
