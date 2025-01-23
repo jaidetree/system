c86a15a6c63a8a4a99dc2a5fd31974941ee4b91f
jaide <jayzawrotny@gmail.com>
2025-01-22 19:17:40 -0500

    Added editorconfig nix file

---
+++ nix/home/editorconfig.nix
@@ -0,0 +1,31 @@
+{ ... }:
+{
+  editorconfig = {
+    enable = true;
+    settings = {
+      "*" = {
+        charset = "utf-8";
+        end_of_line = "lf";
+        insert_final_newline = true;
+        trim_trailing_whitespace = true;
+        max_line_length = 0;
+      };
+
+      "*.{js,ts,jsx,tsx,ts,py,css,scss}" = {
+        indent_style = "tab";
+        indent_size = 2;
+        max_line_length = 80;
+      };
+
+      "*.{res,resi}" = {
+        indent_style = "tab";
+        indent_size = 2;
+        max_line_length = 0;
+      };
+
+      "{package.json,*.yml}" = {
+        indent_style = "space";
+      };
+    };
+  };
+}
