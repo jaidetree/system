6840a4ccfddd1a81fe584254d139dcdb9c2a724e
jaide <jayzawrotny@gmail.com>
2025-03-30 18:47:17 -0400

    Update various config files

--- nix/home/git.nix
+++ nix/home/git.nix
@@ -10,6 +10,7 @@
     enable = true;
     userName = "jaide";
     userEmail = "jayzawrotny@gmail.com";
+
     ignores = [
       ".DS_Store"
       ".envrc"
@@ -24,49 +25,102 @@
       "**/*.secret.*"
       "**/*.j.*"
     ];
+
     aliases = {
       co = "checkout --ignore-other-worktrees";
     };
+
     lfs.enable = true;
+
     signing = {
       signByDefault = true;
       key = "/Users/j/.ssh/id_ed25519.pub";
     };
+
+    # See https://blog.gitbutler.com/how-git-core-devs-configure-git/ as a guide
     extraConfig = {
       init.defaultBranch = "main";
+
+      branch = {
+        autoSetupMerge = "always";
+        sort = "-committerdate";
+      };
+
+      checkout = {
+        defaultRemote = "current";
+        ignoreOtherWorktrees = true;
+      };
+
+      commit = {
+        verbose = true;
+      };
+
+      column.ui = "auto";
+
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+
+      github = {
+        user = "jaidetree";
+      };
+
+      gpg.format = "ssh";
+
+      help.autocorrect = "prompt";
+
+      merge.conflictstyle = "zdiff3";
+
       pull = {
         default = "current";
         rebase = true;
       };
+
       push = {
-        default = "current";
+        default = "simple";
         autoSetupRemote = true;
+        followTags = true;
       };
+
       rebase = {
+        autoSquash = true;
         autoStash = true;
         updateRefs = true;
       };
-      branch = {
-        autoSetupMerge = "always";
+
+      rerere = {
+        enabled = true;
+        autoupdate = true;
       };
-      github = {
-        user = "jaidetree";
+
+      tag = {
+        sort = "version:refname";
       };
-      gpg.format = "ssh";
+
+      # Filters
+
       "filter \"media\"" = {
         required = true;
         clean = "git media clean %f";
         smudge = "git media smudge %f";
       };
+
+
       "filter \"lfs\"" = {
         required = true;
         clean = "git-lfs clean %f";
         smudge = "git-lfs smudge %f";
       };
-      checkout = {
-        defaultRemote = "current";
-        ignoreOtherWorktrees = true;
-      };
+
     };
   };
 }
