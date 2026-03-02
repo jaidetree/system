af50137d89c179023055143601fcdbb6f8268855
jaide <jayzawrotny@gmail.com>
2026-03-02 15:48:21 -0500

    Move dotfiles.nix to host-specific configs
    - Move dotfiles.nix from shared nix/home/ to personal hosts
    - j-bakotsu-mbp and j-oni-mbp now have host-specific dotfiles.nix
    - CGGK727W04 (work machine) no longer conflicts with SSH config
    - Dropbox SSH config and keyboard layout only apply to personal machines

---
+++ nix/hosts/j-oni-mbp/home/dotfiles.nix
@@ -0,0 +1,21 @@
+{ pkgs, config, lib, ... }:
+{
+  # Note: Dotfiles in ~/.config are managed by bin/link.py (called by rebuild.sh)
+  # This file only contains special-case symlinks that don't fit the standard pattern
+
+  home.file = {
+    # SSH config from Dropbox sync
+    "${config.home.homeDirectory}/.ssh/config" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Library/CloudStorage/Dropbox/Sync/ssh/config";
+    };
+
+    # Keyboard layout (needs to be in /Library)
+    "/Library/Keyboard Layouts/Colemak DH.bundle" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/vendor/Colemak DH.bundle";
+    };
+  };
+  home.activation.copyKeyboardLayout = lib.hm.dag.entryBetween [ "linkGeneration" ] [ "writeBoundary" ] ''
+    ${pkgs.zsh}/bin/zsh -c "sudo cp -r ~/.config/vendor/Colemak\ DH.bundle /Library/Keyboard\ Layouts/"
+  '';
+
+}
