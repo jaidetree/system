9ebe4b8446a7050ba9ada8a496047de78c64afe6
jaide <jayzawrotny@gmail.com>
2026-03-26 23:06:04 +0000

    Move npmrc out of nix

--- nix/modules/home-manager/common/node.nix
+++ nix/modules/home-manager/common/node.nix
@@ -13,9 +13,9 @@
       source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/nix/secrets/ngrok.yml";
     };
 
-    "${config.home.homeDirectory}/.npmrc".text = ''
-      prefix=~/.config/npm-global
-    '';
+    # "${config.home.homeDirectory}/.npmrc".text = ''
+    #   prefix=~/.config/npm-global
+    # '';
 
     "${config.home.homeDirectory}/node_modules" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/npm-global/lib/node_modules";
